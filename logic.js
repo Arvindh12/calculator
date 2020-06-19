83904293158006933f1985904ee0a4ae0f866cba
Arvindh12 <arvindhashok1112@gmail.com>
2020-06-20 02:29:02 +0530

    keboard press logic implemented

--- logic.js
+++ logic.js
@@ -1,1 +1,26 @@
-document.addEventListener("click",(e)=>console.log(e))
+var mytags = document.querySelectorAll(".col-3")
+console.log(document.querySelectorAll(".col-3"))
+mytags.forEach((e)=> e.addEventListener("click",(e1)=> displaytext(e1.target.innerText)))
+document.onkeypress= (e) => displaytext(e.key)
+function displaytext(val){
+    var con = document.getElementById("dispbox");
+    console.log(val,"q",con.innerText);
+    if(val==="=" || val==="Enter" ){
+       console.log((eval(con.innerText)))
+       con.innerText = String(eval(con.innerText))
+    }
+    else if(val=="AC"|| val=="d" || val=="D")
+    {
+        con.innerText=""
+    }
+    else if(val=="C" || val=="c")
+    {
+        con.innerText= con.innerText.slice(0,con.innerText.length-1)
+    }
+    else
+    {
+        con.innerText+=val
+        console.log("here")
+    }
+
+}
